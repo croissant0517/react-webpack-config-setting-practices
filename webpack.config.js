a74a5b38effb36f6ae07c1d1e771f893bfc8be24
croissant <wathyic@gmail.com>
2022-01-21 11:33:04 +0800

    just practices how to set up webpack config for react basic

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            {
+                test: /\.(css|scss)$/,
+                // 加載順序由右到左
+                use: ["style-loader", "css-loader", "sass-loader"]
+            },
+            {
+                test: /\.html$/,
+                use: {
+                    loader: "html-loader"
+                }
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./index.html"
+        })
+    ]
+}
